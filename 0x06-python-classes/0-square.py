4afc2e5a7f03d498274555f3250f4f50513b66bf
Moriangomomanyi <moriangomomanyi02@gmail.com>
2024-01-23 01:32:19 -0800

    0x06-python-classes

---
+++ 0x06-python-classes/0-square.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Square Class
+
+This class don't do anything at the moment
+
+"""
+
+class Square:
+    pass
+
---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+"""
+this class square defines a square by is size
+
+"""
+
+class Square:
+
+    def _init_(self, size):
+        """ _init_ method iniatializes the size value of the square.
+
+        Attributes:
+        size (int): the size of the square.
+
+         """
+
+        self._size = size
