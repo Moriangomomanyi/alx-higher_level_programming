721df1fa9cd7b990599b9443ad1d4be5ba85e254
Moriangomomanyi <moriangomomanyi02@gmail.com>
2024-05-22 15:18:12 +0300

    introduction to sql

---
+++ 0x0D-SQL_introduction/101-avg_temperatures.sql
@@ -0,0 +1,2 @@
+-- Execute: cat 101-avg_temperatures.sql | mysql -hlocalhost -uroot -p hbtn_0c_0
+SELECT city, AVG(value) AS avg_temp FROM temperatures GROUP BY city ORDER BY avg_temp DESC;
---
+++ 0x0D-SQL_introduction/2-remove_database.sql
@@ -0,0 +1,2 @@
+tabase.sql-- Remove database if exists
+DROP DATABASE IF EXISTS hbtn_0c_0;
